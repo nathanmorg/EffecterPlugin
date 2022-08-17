08928a68e8aefc54ee0bc723b824be91dcd1e5cc
nathanielmorgana <71456951+nathanielmorgana@users.noreply.github.com>
2022-08-16 23:04:56 -0500

    Modified the login handler to produce random effects for a logged in player event as strings

--- src/main/java/n8/effecter/handlers/loginHandler.java
+++ src/main/java/n8/effecter/handlers/loginHandler.java
@@ -2,19 +2,48 @@ package n8.effecter.handlers;
 
 import n8.effecter.Effecter;
 import org.bukkit.Bukkit;
-import org.bukkit.Effect;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
-import org.bukkit.potion.PotionEffect;
+
+import java.util.Random;
 
 public class loginHandler implements Listener {
     public loginHandler(Effecter plugin) { Bukkit.getPluginManager().registerEvents(this, plugin); }
-    //Need list of possible effects
 
+    //Import list of possible effects
+    //Temporary list of string arrays
+    String[] effectType = {"Negative", "Positive"};
+    String[] posEffects = {"Strength", "Speed", "Regen", "Fire Res"};
+    String[] negEffects = {"Damage", "Poison", "Wither", "Blindness"};
+
+    @EventHandler
     private void onPlayerLogin (PlayerLoginEvent event) {
-    //Need to pick random array either positive or negative
+        Player p = event.getPlayer();
+        Bukkit.broadcastMessage(p.toString());
 
-    // Need to pick random effect within array of positive and negative effects
+        //Need to pick random array either positive or negative
+        String type = getRandomEffectType(effectType);
 
+        //Need to pick random effect within array of positive and negative effects
+        if (type.equals("Negative")) {
+            Bukkit.broadcastMessage(getRandomEffect(negEffects));
+        }
+        else {
+            Bukkit.broadcastMessage(getRandomEffect(posEffects));
+        }
+    }
+
+    //Randomly decide between positive or negative
+    private static String getRandomEffectType(String[] array) {
+        int rnd = new Random().nextInt(array.length);
+        return array[rnd];
+    }
+
+    //Get random element of effects arrays
+    private static String getRandomEffect(String[] array) {
+        int rnd = new Random().nextInt(array.length);
+        return array[rnd];
     }
 }
