f049e9b03d3e3ea551dda44580b9c43ab2524375
nathanielmorgana <71456951+nathanielmorgana@users.noreply.github.com>
2022-08-21 14:46:37 -0500

    Created base class createEffect for createeffect commmand.

---
+++ src/main/java/n8/effecter/commands/createEffect.java
@@ -0,0 +1,17 @@
+package n8.effecter.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class createEffect implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("You are not allowed to run this command from this interface.");
+            return true;
+        }
+        return true;
+    }
+}
